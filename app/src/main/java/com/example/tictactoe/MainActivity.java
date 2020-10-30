533096380f44a7163db795c639a24c63773e4f9e
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-29 19:52:42 -0700

    Renamed ids for all the buttons and textview appropriately

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,8 +3,28 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // game buttons //
+    // top row
+    private Button TopLeftBtn;
+    private Button TopCenterBtn;
+    private Button TopRightBtn;
+    // middle row
+    private Button MiddleLeftBtn;
+    private Button MiddleCenterBtn;
+    private Button MiddleRightBtn;
+    // bottom row
+    private Button BottomLeftBtn;
+    private Button BottomCenterBtn;
+    private Button BottomRightBtn;
+
+    Button[] buttons = {TopLeftBtn, TopCenterBtn, TopRightBtn,
+                        MiddleLeftBtn, MiddleCenterBtn, MiddleRightBtn,
+                        BottomLeftBtn, BottomCenterBtn, BottomRightBtn};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
